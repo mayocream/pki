a0220829893baae7c8855316644b5b812b3f7c07
Mayo <mayocream39@yahoo.co.jp>
2021-09-06 13:30:35 +0800

    chore: tinygo build

--- wasm/pki/certinfo.go
+++ wasm/pki/certinfo.go
@@ -2,38 +2,27 @@ package main
 
 import (
 	"encoding/pem"
-	"syscall/js"
 
 	"github.com/google/certificate-transparency-go/x509"
 	"github.com/mayocream/pki/pkg/x509util"
 )
 
 //export certificateToString
-func certificateToString(this js.Value, args []js.Value) interface{} {
-	certPEM := args[0].String()
+func certificateToString(certPEM string) interface{} {
+    done := make(chan struct{}, 0)
 	block, _ := pem.Decode([]byte(certPEM))
 	if block == nil {
-		return js.Null()
+		return nil
 	}
 	cert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
-		return js.Null()
+		return nil
 	}
-	info := x509util.CertificateToString(cert)
-	return info
-}
-
-//export certificateRequestToString
-func certificateRequestToString(this js.Value, args []js.Value) interface{} {
-	certPEM := args[0].String()
-	block, _ := pem.Decode([]byte(certPEM))
-	if block == nil {
-		return js.Null()
-	}
-	csr, err := x509.ParseCertificateRequest(block.Bytes)
-	if err != nil {
-		return js.Null()
-	}
-	info := x509util.CertificateRequestToString(csr)
+    var info interface{}
+	go func() {
+		info = x509util.CertificateToString(cert)
+        done <- struct{}{}
+	}()
+    <-done
 	return info
 }
--- wasm/pki/main.go
+++ wasm/pki/main.go
@@ -1,17 +1,13 @@
-// +build js,wasm
 package main
-
-//go:generate cp $GOROOT/misc/wasm/wasm_exec.js .
-
-import (
-	"syscall/js"
-)
 
 // This calls a JS function from Go.
 func main() {
-	js.Global().Set("PKI_certificateToString", js.FuncOf(certificateToString))
-	js.Global().Set("PKI_certificateRequestToString", js.FuncOf(certificateRequestToString))
-
 	println("PKI WASM running.")
-	select {}
+}
+
+// This function is exported to JavaScript, so can be called using
+// exports.multiply() in JavaScript.
+//export multiply
+func multiply(x, y int) int {
+    return x * y;
 }
